e3ac489a7e0736dd172d19cfca12f064cf65042c
Greg Burd <gburd@humio.com>
2019-08-05 12:51:03 -0400

    Re-organize and sync with proposed LDAP auth changes in gburd/ldap-issues branch pre-merge to master.

--- src/main/scala/com/humio/ldap_test/Main.scala
+++ src/main/scala/com/humio/ldap_test/Main.scala
@@ -12,36 +12,6 @@ import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 
-
-case class AuthProviderProfile(
-  provider: String,
-  authProviderUsername: String,
-  humioUsername: String,
-  email: Option[Option[String]] = None,
-  company: Option[Option[String]] = None,
-  location: Option[Option[String]] = None,
-  name: Option[Option[String]] = None,
-  isRoot: Option[Option[Boolean]] = None,
-  picture: Option[Option[String]] = None)
-
-object AuthenticationMethod extends Enumeration {
-  val NoAuthentication, SingleUser, Auth0, Ldap, LdapSearch, Static, SetByProxy, FederatedIdentity, OAuth, SAML = Value
-}
-
-case class LdapAuthConfig(
-  ldapAuthProviderUrl: Option[String],
-  ldapAuthProviderCert: Option[String],
-  ldapDomainName: Option[String],
-  ldapAuthPrincipal: Option[String],
-  ldapAuthPrincipalsRegex: Option[String],
-  ldapSearchBindName: Option[String],
-  ldapSearchBindPassword: Option[String],
-  ldapSearchBaseDN: Option[String],
-  ldapSearchDomainName: Option[String],
-  ldapSearchFilter: Option[String],
-  ldapGroupBaseDN: Option[String],
-  ldapGroupFilter: Option[String])
-
 sealed abstract class LdapConnectionMethod(url: String) {
   def env(): Map[String, String] = {
     Map(
@@ -71,10 +41,6 @@ case class SecuredLdapConnection(url: String, cert: Option[String] = None) exten
   }
 }
 
-object LocalLogin {
-  val loginFailure = false
-}
-
 sealed abstract class LdapPrincipalLookup
 case class LdapNamedPrincipal(domain: String, principal: String) extends LdapPrincipalLookup
 case class LdapPrincipalSearch(username: String, password: String, dn: String, filter: String) extends LdapPrincipalLookup
@@ -83,71 +49,9 @@ case class LdapAuth(conn: LdapConnectionMethod, principals: Seq[LdapPrincipalLoo
 
 case class LdapGroupLookup(dn: String, filter: String)
 
-object Main {
-
-  val logger = createLoggerFor("ldap-test")
-
-  def main(args: Array[String]): Unit = {
-
-    if (args.length < 2) {
-      logger.info(
-        s"""
-           |Usage: java -jar ldap-test.jar <username> <password>
-           |    environment variable     =>     example value
-           |    AUTHENTICATION_METHOD           ldap|ldap-search
-           |    LDAP_DOMAIN_NAME                example.com
-           |    LDAP_AUTH_PROVIDER_URL          ldap://ldap.example.com or ldaps: for TLS
-           |    LDAP_AUTH_PROVIDER_CERT         LDAP_AUTH_PROVIDER_CERT=-----BEGIN CERTIFICATE-----\\nMII...gWc=\\n-----END CERTIFICATE-----\\n
-           |    LDAP_AUTH_PRINCIPAL             cn=HUMIOUSERNAME,dc=example,dc=com;cn=HUMIOUSERNAME,dc=foo,dc=bar
-           |    LDAP_AUTH_PRINCIPALS_REGEX      ;
-           |    LDAP_SEARCH_BIND_NAME           cn=Bind User,dc=example,dc=com
-           |    LDAP_SEARCH_BIND_PASSWORD       sekret,pa55w0rd!
-           |    LDAP_SEARCH_DOMAIN_NAME         example.com
-           |    LDAP_SEARCH_BASE_DN             ou=DevOps,dc=example,dc=com
-           |    LDAP_SEARCH_FILTER              (& (userPrincipalName={0})(objectCategory=user))
-           |    LDAP_GROUP_BASE_DN              ou=User administration,dc=example,dc=com
-           |    LDAP_GROUP_FILTER               (& (objectClass=group) (member:1.2.840.113556.1.4.1941:={0}))
-           |
-           |    Phase 1: Determine DN we need to authenticate.  When not using LdapSearch this is constructed from the
-           |    login username and the LDAP_AUTH_PRINCIPAL(s).  Each one is tried in turn combined with the password.
-           |    When LdapSearch is used there is a phase before this.
-           |
-           |    Phase 0.5: LDAP search is used to find the DN for the user by first logging into the LDAP server with
-           |    a well known LDAP_SEARCH_BIND_NAME/PASSWORD.  The goal is to find the DN for the login user within
-           |    the LDAP directory server, then authenticate that against the password provided.
-           |
-           |    Phase 2: Assuming the DN/password authenticated next up is determining if the user belongs to any
-           |    groups.  The group membership is used for RBAC.
-           |
-           |    Requires Java 11 or later.tig
-         """.stripMargin)
-    } else {
-      logger.info(s"Testing LDAP login for user=${args(0)}")
-
-      if (login(args(0), args(1))) {
-        logger.info(s"Fantastic, that seems to have worked.")
-      } else {
-        logger.info(s"Things didn't go as planned, keep trying!")
-      }
-    }
-  }
-
-//  private def ldapConfigFromEnv(kind: AuthenticationMethod.Value): Option[LdapAuthConfig] = {
-  private def ldapConfigFromEnv(): Option[LdapAuthConfig] = {
-    Some(LdapAuthConfig(
-      ldapDomainName = sys.env.get("LDAP_DOMAIN_NAME"),
-      ldapAuthProviderUrl = sys.env.get("LDAP_AUTH_PROVIDER_URL"),
-      ldapAuthProviderCert = sys.env.get("LDAP_AUTH_PROVIDER_CERT"),
-      ldapAuthPrincipal = sys.env.get("LDAP_AUTH_PRINCIPAL"),
-      ldapAuthPrincipalsRegex = sys.env.get("LDAP_AUTH_PRINCIPALS_REGEX"),
-      ldapSearchBindName = sys.env.get("LDAP_SEARCH_BIND_NAME"),
-      ldapSearchBindPassword = sys.env.get("LDAP_SEARCH_BIND_PASSWORD"),
-      ldapSearchDomainName = sys.env.get("LDAP_SEARCH_DOMAIN_NAME"),
-      ldapSearchBaseDN = sys.env.get("LDAP_SEARCH_BASE_DN"),
-      ldapSearchFilter = sys.env.get("LDAP_SEARCH_FILTER"),
-      ldapGroupBaseDN = sys.env.get("LDAP_GROUP_BASE_DN"),
-      ldapGroupFilter = sys.env.get("LDAP_GROUP_FILTER")))
-  }
+//class LdapBindLocalLogin(loginService: LoginService, config: Config) extends LocalLogin {
+object LdapBindLocalLogin {
+  private val logger = Logger.forObject(this)
 
   def login(username: String, pass: String): Boolean = {
     if (username.isBlank) {
@@ -204,7 +108,7 @@ object Main {
       uri match {
         case Some(url) if url.startsWith("ldap:") => UnsecuredLdapConnection(url)
         case Some(url) if url.startsWith("ldaps:") => SecuredLdapConnection(url, ldapAuthConfig.ldapAuthProviderCert)
-        case None => throw new Exception(s"The LDAP authentication URL is missing or incorrect. LDAP_AUTH_PROVIDER_URL=${uri}")
+        case None => throw new HumioException(s"The LDAP authentication URL is missing or incorrect. LDAP_AUTH_PROVIDER_URL=${uri}")
       },
       (ldapAuthConfig.ldapAuthPrincipal match {
         case Some(principals) if principals.nonEmpty && splitPrincipals.isDefined =>
@@ -245,7 +149,7 @@ object Main {
         javax.naming.Context.SECURITY_CREDENTIALS -> secret)).asJava)
   }
 
-  private def openLdapContext(username: String, password: String, url: String, ldapAuth: LdapAuth, ldapGroupBaseDN: Option[String], ldapGroupFilter: Option[String]): (Option[String], Seq[String])= {
+  private def openLdapContext(username: String, password: String, url: String, ldapAuth: LdapAuth, ldapGroupBaseDN: Option[String], ldapGroupFilter: Option[String]): (Option[String], Seq[String]) = {
     logger.info(s"ldap login for user=${username} starting...")
     var ctx: InitialDirContext = null
 
@@ -289,7 +193,7 @@ object Main {
             false
           case e: Throwable =>
             logger.warn(s"authentication failed: ${e}")
-            rethrowIfFatal(e)
+            ExceptionUtils.rethrowIfFatal(e)
             false
         }
       } => Some((ctx, dn))
@@ -328,17 +232,94 @@ object Main {
     principalName
   }
 
-  def rethrowIfFatal(e: Throwable): Unit = {
-    e match {
-      case e: Error => throw e
-      // Other cases that need rethrow to start shutdown in particular?
-      case _ => //fine.
+  //  private def ldapConfigFromEnv(kind: AuthenticationMethod.Value): Option[LdapAuthConfig] = {
+  private def ldapConfigFromEnv(): Option[LdapAuthConfig] = {
+      Some(LdapAuthConfig(
+        ldapDomainName = sys.env.get("LDAP_DOMAIN_NAME"),
+        ldapAuthProviderUrl = sys.env.get("LDAP_AUTH_PROVIDER_URL"),
+        ldapAuthProviderCert = sys.env.get("LDAP_AUTH_PROVIDER_CERT"),
+        ldapAuthPrincipal = sys.env.get("LDAP_AUTH_PRINCIPAL"),
+        ldapAuthPrincipalsRegex = sys.env.get("LDAP_AUTH_PRINCIPALS_REGEX"),
+        ldapSearchBindName = sys.env.get("LDAP_SEARCH_BIND_NAME"),
+        ldapSearchBindPassword = sys.env.get("LDAP_SEARCH_BIND_PASSWORD"),
+        ldapSearchDomainName = sys.env.get("LDAP_SEARCH_DOMAIN_NAME"),
+        ldapSearchBaseDN = sys.env.get("LDAP_SEARCH_BASE_DN"),
+        ldapSearchFilter = sys.env.get("LDAP_SEARCH_FILTER"),
+        ldapGroupBaseDN = sys.env.get("LDAP_GROUP_BASE_DN"),
+        ldapGroupFilter = sys.env.get("LDAP_GROUP_FILTER")))
+    }
+
+}
+
+object Main {
+
+  val logger = Logger.forObject(this)
+
+  def main(args: Array[String]): Unit = {
+
+    if (args.length < 2) {
+      logger.info(
+        s"""
+           |Usage: java -jar ldap-test.jar <username> <password>
+           |    environment variable     =>     example value
+           |    AUTHENTICATION_METHOD           ldap|ldap-search
+           |    LDAP_DOMAIN_NAME                example.com
+           |    LDAP_AUTH_PROVIDER_URL          ldap://ldap.example.com or ldaps: for TLS
+           |    LDAP_AUTH_PROVIDER_CERT         LDAP_AUTH_PROVIDER_CERT=-----BEGIN CERTIFICATE-----\\nMII...gWc=\\n-----END CERTIFICATE-----\\n
+           |    LDAP_AUTH_PRINCIPAL             cn=HUMIOUSERNAME,dc=example,dc=com;cn=HUMIOUSERNAME,dc=foo,dc=bar
+           |    LDAP_AUTH_PRINCIPALS_REGEX      ;
+           |    LDAP_SEARCH_BIND_NAME           cn=Bind User,dc=example,dc=com
+           |    LDAP_SEARCH_BIND_PASSWORD       sekret,pa55w0rd!
+           |    LDAP_SEARCH_DOMAIN_NAME         example.com
+           |    LDAP_SEARCH_BASE_DN             ou=DevOps,dc=example,dc=com
+           |    LDAP_SEARCH_FILTER              (& (userPrincipalName={0})(objectCategory=user))
+           |    LDAP_GROUP_BASE_DN              ou=User administration,dc=example,dc=com
+           |    LDAP_GROUP_FILTER               (& (objectClass=group) (member:1.2.840.113556.1.4.1941:={0}))
+           |
+           |    Phase 1: Determine DN we need to authenticate.  When not using LdapSearch this is constructed from the
+           |    login username and the LDAP_AUTH_PRINCIPAL(s).  Each one is tried in turn combined with the password.
+           |    When LdapSearch is used there is a phase before this.
+           |
+           |    Phase 0.5: LDAP search is used to find the DN for the user by first logging into the LDAP server with
+           |    a well known LDAP_SEARCH_BIND_NAME/PASSWORD.  The goal is to find the DN for the login user within
+           |    the LDAP directory server, then authenticate that against the password provided.
+           |
+           |    Phase 2: Assuming the DN/password authenticated next up is determining if the user belongs to any
+           |    groups.  The group membership is used for RBAC.
+           |
+           |    Requires Java 11 or later.tig
+         """.stripMargin)
+    } else {
+      logger.info(s"Testing LDAP login for user=${args(0)}")
+
+      if (LdapBindLocalLogin.login(args(0), args(1))) {
+        logger.info(s"Fantastic, that seems to have worked.")
+      } else {
+        logger.info(s"Things didn't go as planned, keep trying!")
+      }
     }
   }
 
-  def createLoggerFor(string: String, file: Option[String] = None): ch.qos.logback.classic.Logger = {
+}
+
+object ExceptionUtils {
+    def rethrowIfFatal(e: Throwable): Unit = {
+      e match {
+        case e: Error => throw e
+        // Other cases that need rethrow to start shutdown in particular?
+        case _ => //fine.
+      }
+    }
+}
+
+object Logger {
+  def forObject(o: Any): ch.qos.logback.classic.Logger = {
+    buildLogger(o.getClass.getCanonicalName)
+  }
+
+  def buildLogger(string: String, file: Option[String] = None): ch.qos.logback.classic.Logger = {
     val ple = new PatternLayoutEncoder()
-    val lc = LoggerFactory.getILoggerFactory().asInstanceOf[LoggerContext]
+    val lc = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
     ple.setPattern("%date %level [%thread] %logger{10} [%file:%line]\t%msg%n")
     ple.setContext(lc)
     ple.start()
@@ -363,5 +344,40 @@ object Main {
 
     logger
   }
-
 }
+
+class HumioException(msg: String) extends Exception
+
+object LocalLogin {
+  val loginFailure = false
+}
+
+case class AuthProviderProfile(
+  provider: String,
+  authProviderUsername: String,
+  humioUsername: String,
+  email: Option[Option[String]] = None,
+  company: Option[Option[String]] = None,
+  location: Option[Option[String]] = None,
+  name: Option[Option[String]] = None,
+  isRoot: Option[Option[Boolean]] = None,
+  picture: Option[Option[String]] = None)
+
+object AuthenticationMethod extends Enumeration {
+  val NoAuthentication, SingleUser, Auth0, Ldap, LdapSearch, Static, SetByProxy, FederatedIdentity, OAuth, SAML = Value
+}
+
+case class LdapAuthConfig(
+  ldapAuthProviderUrl: Option[String],
+  ldapAuthProviderCert: Option[String],
+  ldapDomainName: Option[String],
+  ldapAuthPrincipal: Option[String],
+  ldapAuthPrincipalsRegex: Option[String],
+  ldapSearchBindName: Option[String],
+  ldapSearchBindPassword: Option[String],
+  ldapSearchBaseDN: Option[String],
+  ldapSearchDomainName: Option[String],
+  ldapSearchFilter: Option[String],
+  ldapGroupBaseDN: Option[String],
+  ldapGroupFilter: Option[String])
+
